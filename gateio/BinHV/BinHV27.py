419ea26d933bd78530f7af219ed2b024d47ffc19
E0V1E <741033569@qq.com>
2022-04-01 15:11:33 +0800

    change BinHV27 to hyper able.

--- gateio/BinHV/BinHV27.py
+++ gateio/BinHV/BinHV27.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-from freqtrade.strategy import DecimalParameter, stoploss_from_open
+from freqtrade.persistence import Trade
+from freqtrade.strategy import DecimalParameter, stoploss_from_open, CategoricalParameter, IntParameter
 from freqtrade.strategy.interface import IStrategy
 from pandas import DataFrame
 # --------------------------------
@@ -17,19 +18,43 @@ class BinHV27(IStrategy):
     """
 
     minimal_roi = {
-        "0": 1
+        "0": 100
+    }
+
+    buy_params = {
+        'buy_adx1': 25,
+        'buy_emarsi1': 20,
+        'buy_adx2': 30,
+        'buy_emarsi2': 20,
+        'buy_adx3': 35,
+        'buy_emarsi3': 20,
+        'buy_adx4': 30,
+        'buy_emarsi4': 25
     }
 
     sell_params = {
-        # custom stoploss params, come from BB_RPB_TSL
+        # custom stop loss params
         "pHSL": -0.25,
         "pPF_1": 0.023,
         "pPF_2": 0.042,
         "pSL_1": 0.018,
-        "pSL_2": 0.041
+        "pSL_2": 0.041,
+
+        # sell params
+        'emarsi1': 75,
+        'adx2': 30,
+        'emarsi2': 80,
+        'emarsi3': 75,
+
+        # sell optional
+        "sell_1": True,
+        "sell_2": True,
+        "sell_3": True,
+        "sell_4": True,
+        "sell_5": True,
     }
 
-    stoploss = -0.99
+    stoploss = -0.25
     timeframe = '5m'
 
     # Make sure these match or are not overridden in config
@@ -38,7 +63,7 @@ class BinHV27(IStrategy):
     ignore_roi_if_buy_signal = False
 
     # Custom stoploss
-    use_custom_stoploss = True
+    use_custom_stoploss = False
 
     process_only_new_candles = True
     startup_candle_count = 240
@@ -56,17 +81,36 @@ class BinHV27(IStrategy):
         'stoploss_on_exchange_limit_ratio': 0.99
     }
 
-    # hard stoploss profit
-    pHSL = DecimalParameter(-0.990, -0.040, default=-0.08, decimals=3, space='sell')
-    # profit threshold 1, trigger point, SL_1 is used
-    pPF_1 = DecimalParameter(0.008, 0.050, default=0.016, decimals=3, space='sell')
-    pSL_1 = DecimalParameter(0.008, 0.050, default=0.011, decimals=3, space='sell')
+    # buy params
+    buy_adx1 = IntParameter(low=10, high=100, default=25, space='buy', optimize=True)
+    buy_emarsi1 = IntParameter(low=10, high=100, default=20, space='buy', optimize=True)
+    buy_adx2 = IntParameter(low=20, high=100, default=30, space='buy', optimize=True)
+    buy_emarsi2 = IntParameter(low=20, high=100, default=20, space='buy', optimize=True)
+    buy_adx3 = IntParameter(low=10, high=100, default=35, space='buy', optimize=True)
+    buy_emarsi3 = IntParameter(low=10, high=100, default=20, space='buy', optimize=True)
+    buy_adx4 = IntParameter(low=20, high=100, default=30, space='buy', optimize=True)
+    buy_emarsi4 = IntParameter(low=20, high=100, default=25, space='buy', optimize=True)
 
-    # profit threshold 2, SL_2 is used
-    pPF_2 = DecimalParameter(0.040, 0.100, default=0.080, decimals=3, space='sell')
-    pSL_2 = DecimalParameter(0.040, 0.100, default=0.040, decimals=3, space='sell')
+    # trailing stop loss
+    pHSL = DecimalParameter(-0.990, -0.040, default=-0.08, decimals=3, space='sell', optimize=False)
+    pPF_1 = DecimalParameter(0.008, 0.050, default=0.016, decimals=3, space='sell', optimize=False)
+    pSL_1 = DecimalParameter(0.008, 0.050, default=0.011, decimals=3, space='sell', optimize=False)
+    pPF_2 = DecimalParameter(0.040, 0.100, default=0.080, decimals=3, space='sell', optimize=False)
+    pSL_2 = DecimalParameter(0.040, 0.100, default=0.040, decimals=3, space='sell', optimize=False)
 
-    def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
+    # sell params
+    adx2 = IntParameter(low=10, high=100, default=30, space='sell', optimize=True)
+    emarsi1 = IntParameter(low=10, high=100, default=75, space='sell', optimize=True)
+    emarsi2 = IntParameter(low=20, high=100, default=80, space='sell', optimize=True)
+    emarsi3 = IntParameter(low=20, high=100, default=75, space='sell', optimize=True)
+
+    sell_1 = CategoricalParameter([True, False], default=True, space="sell", optimize=True)
+    sell_2 = CategoricalParameter([True, False], default=True, space="sell", optimize=True)
+    sell_3 = CategoricalParameter([True, False], default=True, space="sell", optimize=True)
+    sell_4 = CategoricalParameter([True, False], default=True, space="sell", optimize=True)
+    sell_5 = CategoricalParameter([True, False], default=True, space="sell", optimize=True)
+
+    def custom_stoploss(self, pair: str, trade: Trade, current_time: datetime,
                         current_rate: float, current_profit: float, **kwargs) -> float:
 
         # hard stoploss profit
@@ -127,75 +171,98 @@ class BinHV27(IStrategy):
             dataframe['minusdi'].gt(dataframe['minusdiema']) &
             dataframe['rsi'].ge(dataframe['rsi'].shift()) &
             (
-              (
-                ~dataframe['preparechangetrend'] &
-                ~dataframe['continueup'] &
-                dataframe['adx'].gt(25) &
-                dataframe['bigdown'] &
-                dataframe['emarsi'].le(20)
-              ) |
-              (
-                ~dataframe['preparechangetrend'] &
-                dataframe['continueup'] &
-                dataframe['adx'].gt(30) &
-                dataframe['bigdown'] &
-                dataframe['emarsi'].le(20)
-              ) |
-              (
-                ~dataframe['continueup'] &
-                dataframe['adx'].gt(35) &
-                dataframe['bigup'] &
-                dataframe['emarsi'].le(20)
-              ) |
-              (
-                dataframe['continueup'] &
-                dataframe['adx'].gt(30) &
-                dataframe['bigup'] &
-                dataframe['emarsi'].le(25)
-              )
+                    (
+                            ~dataframe['preparechangetrend'] &
+                            ~dataframe['continueup'] &
+                            dataframe['adx'].gt(self.buy_adx1.value) &
+                            dataframe['bigdown'] &
+                            dataframe['emarsi'].le(self.buy_emarsi1.value)
+                    ) |
+                    (
+                            ~dataframe['preparechangetrend'] &
+                            dataframe['continueup'] &
+                            dataframe['adx'].gt(self.buy_adx2.value) &
+                            dataframe['bigdown'] &
+                            dataframe['emarsi'].le(self.buy_emarsi2.value)
+                    ) |
+                    (
+                            ~dataframe['continueup'] &
+                            dataframe['adx'].gt(self.buy_adx3.value) &
+                            dataframe['bigup'] &
+                            dataframe['emarsi'].le(self.buy_emarsi3.value)
+                    ) |
+                    (
+                            dataframe['continueup'] &
+                            dataframe['adx'].gt(self.buy_adx4.value) &
+                            dataframe['bigup'] &
+                            dataframe['emarsi'].le(self.buy_emarsi4.value)
+                    )
             ),
             'buy'] = 1
+
         return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe.loc[
-            (
-              (
-                ~dataframe['preparechangetrendconfirm'] &
-                ~dataframe['continueup'] &
-                (dataframe['close'].gt(dataframe['lowsma']) | dataframe['close'].gt(dataframe['highsma'])) &
-                dataframe['highsma'].gt(0) &
-                dataframe['bigdown']
-              ) |
-              (
-                ~dataframe['preparechangetrendconfirm'] &
-                ~dataframe['continueup'] &
-                dataframe['close'].gt(dataframe['highsma']) &
-                dataframe['highsma'].gt(0) &
-                (dataframe['emarsi'].ge(75) | dataframe['close'].gt(dataframe['slowsma'])) &
-                dataframe['bigdown']
-              ) |
-              (
-                ~dataframe['preparechangetrendconfirm'] &
-                dataframe['close'].gt(dataframe['highsma']) &
-                dataframe['highsma'].gt(0) &
-                dataframe['adx'].gt(30) &
-                dataframe['emarsi'].ge(80) &
-                dataframe['bigup']
-              ) |
-              (
-                dataframe['preparechangetrendconfirm'] &
-                ~dataframe['continueup'] &
-                dataframe['slowingdown'] &
-                dataframe['emarsi'].ge(75) &
-                dataframe['slowsma'].gt(0)
-              ) |
-              (
-                dataframe['preparechangetrendconfirm'] &
-                dataframe['minusdi'].lt(dataframe['plusdi']) &
-                dataframe['close'].gt(dataframe['lowsma']) &
-                dataframe['slowsma'].gt(0)
-              )
-            ),
-            'sell'] = 1
+        dataframe.loc[(), 'sell'] = 0
         return dataframe
+
+    def custom_sell(self, pair: str, trade: Trade, current_time: 'datetime', current_rate: float,
+                    current_profit: float, **kwargs):
+
+        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+
+        last_candle = dataframe.iloc[-1].squeeze()
+
+        # if current_profit >= 0.023:
+        #     return None
+
+        if self.sell_1.value:
+            if (
+                    (~last_candle['preparechangetrendconfirm'])
+                    and (~last_candle['continueup'])
+                    and (last_candle['close'] > last_candle['lowsma'] or last_candle['close'] > last_candle['highsma'])
+                    and (last_candle['highsma'] > 0)
+                    and (last_candle['bigdown'])
+            ):
+                return "sell_1"
+
+        if self.sell_2.value:
+            if (
+                    (~last_candle['preparechangetrendconfirm'])
+                    and (~last_candle['continueup'])
+                    and (last_candle['close'] > last_candle['highsma'])
+                    and (last_candle['highsma'] > 0)
+                    and (last_candle['emarsi'] > self.emarsi1.value or last_candle['close'] > last_candle['slowsma'])
+                    and (last_candle['bigdown'])
+            ):
+                return "sell_2"
+
+        if self.sell_3.value:
+            if (
+                    (~last_candle['preparechangetrendconfirm'])
+                    and (last_candle['close'] > last_candle['highsma'])
+                    and (last_candle['highsma'] > 0)
+                    and (last_candle['adx'] > self.adx2.value)
+                    and (last_candle['emarsi'] >= self.emarsi2.value)
+                    and (last_candle['bigup'])
+            ):
+                return "sell_3"
+
+        if self.sell_4.value:
+            if (
+                    (last_candle['preparechangetrendconfirm'])
+                    and (~last_candle['continueup'])
+                    and (last_candle['slowingdown'])
+                    and (last_candle['emarsi'] >= self.emarsi3.value)
+                    and (last_candle['slowsma'] > 0)
+            ):
+                return "sell_4"
+
+        if self.sell_5.value:
+            if (
+                    (last_candle['preparechangetrendconfirm'])
+                    and (last_candle['minusdi'] < last_candle['plusdi'])
+                    and (last_candle['close'] > last_candle['lowsma'])
+                    and (last_candle['slowsma'] > 0)
+            ):
+                return "sell_5"
