0e9a9277e68f9f8d61a999bf9a5bf1499fc2791c
stevenz1 <stevenz1@vmware.com>
2022-12-05 17:51:27 +0800

    clean code.

--- binance/E0V1E.py
+++ binance/E0V1E.py
@@ -44,7 +44,7 @@ class E0V1E(IStrategy):
     }
 
     # Disabled
-    stoploss = -0.1
+    stoploss = -0.99
 
     # Custom stoploss
     use_custom_stoploss = True
@@ -56,37 +56,42 @@ class E0V1E(IStrategy):
     buy_ema_low = DecimalParameter(0.9, 0.99, default=0.942, space='buy', optimize=is_optimize_ewo)
     buy_ema_high = DecimalParameter(0.95, 1.2, default=1.084, space='buy', optimize=is_optimize_ewo)
 
-    is_optimize_32 = False
+    is_optimize_32 = True
     buy_rsi_fast_32 = IntParameter(20, 70, default=46, space='buy', optimize=is_optimize_32)
     buy_rsi_32 = IntParameter(15, 50, default=19, space='buy', optimize=is_optimize_32)
     buy_sma15_32 = DecimalParameter(0.900, 1, default=0.942, decimals=3, space='buy', optimize=is_optimize_32)
     buy_cti_32 = DecimalParameter(-1, 0, default=-0.86, decimals=2, space='buy', optimize=is_optimize_32)
 
-    is_optimize_deadfish = False
+    is_optimize_deadfish = True
     sell_deadfish_bb_width = DecimalParameter(0.03, 0.75, default=0.05, space='sell', optimize=is_optimize_deadfish)
     sell_deadfish_profit = DecimalParameter(-0.15, -0.05, default=-0.05, space='sell', optimize=is_optimize_deadfish)
     sell_deadfish_bb_factor = DecimalParameter(0.90, 1.20, default=1.0, space='sell', optimize=is_optimize_deadfish)
     sell_deadfish_volume_factor = DecimalParameter(1, 2.5, default=1.0, space='sell', optimize=is_optimize_deadfish)
 
-    sell_fastx = IntParameter(50, 100, default=75, space='sell', optimize=False)
-    delay_time = IntParameter(90, 1440, default=300, space='sell', optimize=False)
+    sell_fastx = IntParameter(50, 100, default=75, space='sell', optimize=True)
+    delay_time = IntParameter(90, 1440, default=300, space='sell', optimize=True)
     fask_trailing = DecimalParameter(0.001, 0.02, default=0.001, space='sell', optimize=True)
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
-        # NFI 32 indicators
+        # buy_1 indicators
         dataframe['sma_15'] = ta.SMA(dataframe, timeperiod=15)
-
         dataframe['cti'] = pta.cti(dataframe["close"], length=20)
-
         dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
         dataframe['rsi_fast'] = ta.RSI(dataframe, timeperiod=4)
         dataframe['rsi_slow'] = ta.RSI(dataframe, timeperiod=20)
 
+        # ewo indicators
+        dataframe['ema_8'] = ta.EMA(dataframe, timeperiod=8)
+        dataframe['ema_16'] = ta.EMA(dataframe, timeperiod=16)
+        dataframe['EWO'] = ewo(dataframe, 50, 200)
+
+        # profit sell indicators
         stoch_fast = ta.STOCHF(dataframe, 5, 3, 0, 3, 0)
         dataframe['fastd'] = stoch_fast['fastd']
         dataframe['fastk'] = stoch_fast['fastk']
 
+        # loss sell indicators
         bollinger2 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
         dataframe['bb_lowerband2'] = bollinger2['lower']
         dataframe['bb_middleband2'] = bollinger2['mid']
@@ -97,12 +102,6 @@ class E0V1E(IStrategy):
 
         dataframe['volume_mean_12'] = dataframe['volume'].rolling(12).mean().shift(1)
         dataframe['volume_mean_24'] = dataframe['volume'].rolling(24).mean().shift(1)
-
-        # cofi & ewo
-        dataframe['ema_8'] = ta.EMA(dataframe, timeperiod=8)
-        dataframe['ema_16'] = ta.EMA(dataframe, timeperiod=16)
-        # Elliot
-        dataframe['EWO'] = ewo(dataframe, 50, 200)
 
         return dataframe
 
@@ -150,14 +149,14 @@ class E0V1E(IStrategy):
         if current_time - timedelta(minutes=int(self.delay_time.value) * 2) > trade.open_date_utc:
             if current_profit >= -0.02:
                 return -0.006
-        
+
         dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
         current_candle = dataframe.iloc[-1].squeeze()
 
         if current_profit > 0:
             if current_candle["fastk"] > self.sell_fastx.value:
                 return self.fask_trailing.value
-         
+
         return self.stoploss
 
     def custom_exit(self, pair: str, trade: Trade, current_time: datetime, current_rate: float,
@@ -165,10 +164,6 @@ class E0V1E(IStrategy):
 
         dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
         current_candle = dataframe.iloc[-1].squeeze()
-
-        # if current_profit > 0:
-        #     if current_candle["fastk"] > self.sell_fastx.value:
-        #         return "sell_fastk"
 
         # stoploss - deadfish
         if ((current_profit < self.sell_deadfish_profit.value)
