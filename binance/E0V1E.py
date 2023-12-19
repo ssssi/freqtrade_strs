388c8a0f853b82c7c7eef99a6b4598a8673c77a1
E0V1E <evilzzq123@gmail.com>
2023-12-19 11:17:36 +0800

    Update E0V1E.py

--- binance/E0V1E.py
+++ binance/E0V1E.py
@@ -12,12 +12,9 @@ class E0V1E(IStrategy):
     minimal_roi = {
         "0": 1
     }
-
     timeframe = '5m'
-
     process_only_new_candles = True
     startup_candle_count = 120
-
     order_types = {
         'entry': 'market',
         'exit': 'market',
@@ -26,13 +23,11 @@ class E0V1E(IStrategy):
         'force_exit': "market",
         'stoploss': 'market',
         'stoploss_on_exchange': False,
-
         'stoploss_on_exchange_interval': 60,
         'stoploss_on_exchange_market_ratio': 0.99
     }
 
-    stoploss = -0.25
-
+    stoploss = -1
     use_custom_stoploss = True
 
     is_optimize_32 = True
@@ -40,11 +35,10 @@ class E0V1E(IStrategy):
     buy_rsi_32 = IntParameter(15, 50, default=35, space='buy', optimize=is_optimize_32)
     buy_sma15_32 = DecimalParameter(0.900, 1, default=0.961, decimals=3, space='buy', optimize=is_optimize_32)
     buy_cti_32 = DecimalParameter(-1, 0, default=-0.58, decimals=2, space='buy', optimize=is_optimize_32)
-
-    sell_fastx = IntParameter(50, 100, default=75, space='sell', optimize=True)
+    
+    sell_cci = IntParameter(low=50, high=300, default=100, space='sell', optimize=True)
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
         # buy_1 indicators
         dataframe['sma_15'] = ta.SMA(dataframe, timeperiod=15)
         dataframe['cti'] = pta.cti(dataframe["close"], length=20)
@@ -52,17 +46,13 @@ class E0V1E(IStrategy):
         dataframe['rsi_fast'] = ta.RSI(dataframe, timeperiod=4)
         dataframe['rsi_slow'] = ta.RSI(dataframe, timeperiod=20)
 
-        # profit sell indicators
-        stoch_fast = ta.STOCHF(dataframe, 5, 3, 0, 3, 0)
-        dataframe['fastk'] = stoch_fast['fastk']
+        dataframe['cci'] = ta.CCI(dataframe, timeperiod=20)
 
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
         conditions = []
         dataframe.loc[:, 'enter_tag'] = ''
-
         buy_1 = (
                 (dataframe['rsi_slow'] < dataframe['rsi_slow'].shift(1)) &
                 (dataframe['rsi_fast'] < self.buy_rsi_fast_32.value) &
@@ -70,15 +60,12 @@ class E0V1E(IStrategy):
                 (dataframe['close'] < dataframe['sma_15'] * self.buy_sma15_32.value) &
                 (dataframe['cti'] < self.buy_cti_32.value)
         )
-
         conditions.append(buy_1)
         dataframe.loc[buy_1, 'enter_tag'] += 'buy_1'
-
         if conditions:
             dataframe.loc[
                 reduce(lambda x, y: x | y, conditions),
                 'enter_long'] = 1
-
         return dataframe
 
     def custom_stoploss(self, pair: str, trade: Trade, current_time: datetime, current_rate: float,
@@ -86,21 +73,29 @@ class E0V1E(IStrategy):
         dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
         current_candle = dataframe.iloc[-1].squeeze()
 
-        if current_profit >= 0.08:
-            return -0.01
-
-        if current_profit > 0:
-            if current_candle["fastk"] > self.sell_fastx.value:
-                return -0.001
+        if current_candle["cci"] < self.sell_cci.value:
+            if current_profit >= 0.08:
+                return -0.01
 
         if current_time - timedelta(hours=4) > trade.open_date_utc:
-            if current_profit > -0.03:
-                return -0.005
+            self.stoploss = -0.18
 
         return self.stoploss
 
+    def custom_exit(self, pair: str, trade: 'Trade', current_time: 'datetime', current_rate: float,
+                    current_profit: float, **kwargs):
+        dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
+        current_candle = dataframe.iloc[-1].squeeze()
+
+        if current_profit > 0:
+            if current_candle["cci"] >= self.sell_cci.value:
+                return "cci_profit_sell"
+
+        if current_candle["cci"] >= 200:
+            return "cci_loss_sell"
+
+        return None
+
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
         dataframe.loc[(), ['exit_long', 'exit_tag']] = (0, 'long_out')
-
         return dataframe
