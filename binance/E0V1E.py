ffdb856d29e2af6172d3fcec7395720ba027b5e2
E0V1E <evilzzq123@gmail.com>
2024-04-03 10:33:42 +0800

    Update E0V1E.py for latest image

--- binance/E0V1E.py
+++ binance/E0V1E.py
@@ -106,5 +106,5 @@ class E0V1E(IStrategy):
         return None
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe.loc[(), ['exit_long', 'exit_tag']] = (0, 'long_out')
+        dataframe.loc[:, ['exit_long', 'exit_tag']] = (0, 'long_out')
         return dataframe
